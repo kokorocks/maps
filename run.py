ad731f31218ffb22d9c71be9e214004802228a04
Ethan <175902687+kokorocks@users.noreply.github.com>
2025-03-18 20:34:26 +0000

    added html

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from mpmath import mp
-mp.dps = 500000  # Set precision to 50 decimal places
+mp.dps = 50000000  # Set precision to 50 decimal places
 print(mp.pi)
+
+with open("output.html", "w") as file:
+    file.write('<h1>PI</h1><div>'+mp.pi+'</div>')
