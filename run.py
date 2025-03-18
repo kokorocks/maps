9f0edab0561d76d696397333d996cf46f901454f
Ethan <175902687+kokorocks@users.noreply.github.com>
2025-03-18 20:57:05 +0000

    made index.html have pi

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from mpmath import mp
-mp.dps = 50000000  # Set precision to 50 decimal places
-print(mp.pi)
+mp.dps = 500000  # Set precision to 50 decimal places
+#print(mp.pi)
 
-with open("output.html", "w") as file:
-    file.write('<h1>PI</h1><div>'+mp.pi+'</div>')
+output_pi = "<h1>PI</h1><div>"+str(mp.pi)+"</div>"
+
+with open("index.html", "w") as file:
+    file.write(output_pi)
+
